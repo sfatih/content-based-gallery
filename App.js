c279f661969cd9f0846f1d5addc62da14ba2d7e3
sfatih <s_fatihh@hotmail.com>
2018-05-03 13:36:11 +0300

    clean up

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
-/*jshint esversion: 6 */ 
-import React from 'react';
-import { Text, View,  } from 'react-native';
-import {TabNavigator} from 'react-navigation';
-
-import listScreen from './tabs/listScreen';
-import cameraScreen from './tabs/cameraScreen';
-import searchScreen from './tabs/searchScreen';
+import {
+    TabNavigator
+} from "react-navigation";
+import cameraScreen from "./tabs/cameraScreen";
+import searchScreen from "./tabs/searchScreen";
 
 const MainScreenNavigator = TabNavigator({
-    Search: { screen: searchScreen },
-    Camera: { screen: cameraScreen }
-},{
-    swipeEnabled:true,
-    tabBarOptions:{
-        activeTintColor:'#fff',
-        activeBackgroundColor:'#fff',
-        inactiveTintColor:'#2F353B',
-        inactiveBackgroundColor:'#44B6AE'        
+    Search: {
+        screen: searchScreen
+    },
+    Camera: {
+        screen: cameraScreen
+    }
+}, {
+    swipeEnabled: true,
+    tabBarOptions: {
+        activeTintColor: "#fff",
+        activeBackgroundColor: "#fff",
+        inactiveTintColor: "#2F353B",
+        inactiveBackgroundColor: "#44B6AE"
     }
 });
 
